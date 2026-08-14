19c4a2021d2d846d22b8a8cc4aa9013e53669382
Lyshaka <52411658+Lyshaka@users.noreply.github.com>
2026-08-14 17:57:08 +0200

    Added Observable

---
+++ Runtime/Utilities/Observable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ulys.Runtime.Utilities
+{
+
+public class Observable<T>
+{
+	private T _value;
+	
+	public event Action<T> OnValueSet;
+	public event Action<T> OnValueGet;
+
+	public Observable(T value)
+	{
+		_value = value;
+	}
+	
+	public T value
+	{
+		get
+		{
+			OnValueGet?.Invoke(_value);
+			return _value;
+		}
+		set
+		{
+			OnValueSet?.Invoke(value);
+			_value = value;
+		}
+	}
+}
+
+}
