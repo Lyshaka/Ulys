fd21c19c69174a4a16976985420f6b62a57158bc
Lyshaka <52411658+Lyshaka@users.noreply.github.com>
2026-08-15 22:40:03 +0200

    Added Component Extensions

---
+++ Runtime/Extensions/ComponentExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Ulys.Runtime.Extensions
+{
+
+using UnityEngine;
+
+public static class ComponentExtensions
+{
+	/// <summary>
+	/// Tries to get a reference to a component of type <typeparamref name="T"/> on the same GameObject as the component specified, or any parent of the GameObject.
+	/// See <a href="https://docs.unity3d.com/ScriptReference/Component.GetComponentInParent.html">GetComponentInParent</a>.
+	/// </summary>
+	public static bool TryGetComponentInParent<T>(this Component component, out T result, bool includeInactive = false) where T : Component
+	{
+		result = component.GetComponentInParent<T>(includeInactive);
+		return result != null;
+	}
+
+	/// <summary>
+	/// Tries to get a reference to a component of type <typeparamref name="T"/> on the same GameObject as the component specified, or any child of the GameObject.
+	/// See <a href="https://docs.unity3d.com/ScriptReference/Component.GetComponentInChildren.html">GetComponentInChildren</a>.
+	/// </summary>
+	public static bool TryGetComponentInChildren<T>(this Component component, out T result, bool includeInactive = false) where T : Component
+	{
+		result = component.GetComponentInChildren<T>(includeInactive);
+		return result != null;
+	}
+}
+
+}
