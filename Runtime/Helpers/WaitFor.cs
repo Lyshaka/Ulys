4688845fbeb0ef791e0a7ad62be3abe8df02905c
Lyshaka <52411658+Lyshaka@users.noreply.github.com>
2026-09-02 14:23:38 +0200

    Added WaitFor

---
+++ Runtime/Helpers/WaitFor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ulys.Runtime.Helpers
+{
+
+public static class WaitFor
+{
+	public static WaitForFixedUpdate fixedUpdate { get; } = new WaitForFixedUpdate();
+	public static WaitForEndOfFrame endOfFrame { get; } = new WaitForEndOfFrame();
+
+
+	private static readonly Dictionary<float, WaitForSeconds> WaitForSecondsDict = new(100, new FloatComparer());
+
+	public static WaitForSeconds Seconds(float seconds)
+	{
+		if (seconds <= 0f || seconds < (1f / Application.targetFrameRate))
+			return null;
+
+		if (WaitForSecondsDict.TryGetValue(seconds, out WaitForSeconds forSeconds))
+			return forSeconds;
+
+		forSeconds = new(seconds);
+		WaitForSecondsDict.Add(seconds, forSeconds);
+
+		return forSeconds;
+	}
+
+
+	private class FloatComparer : IEqualityComparer<float>
+	{
+		public bool Equals(float x, float y) => Mathf.Abs(x - y) <= float.Epsilon;
+		public int GetHashCode(float obj) => obj.GetHashCode();
+	}
+}
+
+}
